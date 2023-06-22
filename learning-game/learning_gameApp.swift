4e749acca86100aaa35274e2f945ef0c02eda79c
SriramKumar <sriramkrish0110@gmail.com>
2023-06-22 12:35:46 -0700

    Initial Commit

---
+++ learning-game/learning_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_gameApp.swift
+//  learning-game
+//
+//  Created by admin on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct learning_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learning-gameUITests/learning_gameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  learning_gameUITestsLaunchTests.swift
+//  learning-gameUITests
+//
+//  Created by admin on 6/22/23.
+//
+
+import XCTest
+
+final class learning_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

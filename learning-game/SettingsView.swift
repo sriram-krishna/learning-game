8738b947d55d617d00c905e50f286f5f904e0e8f
Vandit Jindal <VanditJindal@my.unt.edu>
2023-07-03 18:11:14 -0500

    "Add profile" button has been added to settings page. User can add a profile and it will show up as a button. Does not function correctly as it saves the users info to the profiles page. Back button has been implemented

--- learning-game/SettingsView.swift
+++ learning-game/SettingsView.swift
@@ -8,125 +8,131 @@
 import Foundation
 import SwiftUI
 
-struct Profile {
- var name: String
- var phoneNumber: String
- var address: String
+struct Profile: Identifiable, Equatable {
+    var id = UUID()
+    var name: String
+    var phoneNumber: String
+    var address: String
+    
+    static func == (lhs: Profile, rhs: Profile) -> Bool{
+        return lhs.id == rhs.id
+    }
 }
-
 struct SettingsView: View {
- @State private var profiles: [Profile] = [
-     Profile(name: "Profile 1", phoneNumber: "", address: ""),
-     Profile(name: "Profile 2", phoneNumber: "", address: ""),
-     Profile(name: "Profile 3", phoneNumber: "", address: "")
- ]
- @State private var selectedProfileIndex = 0
- @State private var showProfiles = false
- @State private var showNewProfilePage = false
- 
- var body: some View {
-     Form {
-         Section(header: Text("Profile Information")) {
-             Button(action: {
-                 showProfiles = true
-             }) {
-                 Text("Profiles")
-                     .font(.title)
-                     .fontWeight(.bold)
-                     .padding()
-                     .background(Color.blue)
-                     .foregroundColor(.white)
-                     .cornerRadius(10)
-             }
-             
-             if showProfiles {
-                 VStack(spacing: 10) {
-                     ForEach(profiles.indices, id: \.self) { index in
-                         Button(action: {
-                             selectedProfileIndex = index
-                             showProfiles = false
-                         }) {
-                             Text(profiles[index].name)
-                                 .font(.title)
-                                 .fontWeight(.bold)
-                                 .padding()
-                                 .background(selectedProfileIndex == index ? Color.blue : Color.gray)
-                                 .foregroundColor(.white)
-                                 .cornerRadius(10)
-                         }
-                     }
-                 }
-             }
-             
-             TextField("Name", text: $profiles[selectedProfileIndex].name)
-             TextField("Phone Number", text: $profiles[selectedProfileIndex].phoneNumber)
-             TextField("Address", text: $profiles[selectedProfileIndex].address)
-         }
-         
-         Section {
-             Button(action: {
-                 showNewProfilePage = true
-             }) {
-                 Text("Profile")
-                     .font(.title)
-                     .fontWeight(.bold)
-                     .padding()
-                     .background(Color.green)
-                     .foregroundColor(.white)
-                     .cornerRadius(10)
-             }
-         }
-     }
-     .sheet(isPresented: $showNewProfilePage) {
-         NewProfileView(profiles: $profiles)
-     }
-     .navigationBarTitle(Text("Settings"))
- }
+    @State private var profiles: [Profile] = []
+    @State private var selectedProfile: Profile?
+    @State private var showNewProfilePage = false
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Profile Information")) {
+                VStack {
+                    if profiles.isEmpty {
+                        Button(action: {
+                            showNewProfilePage = true
+                        }) {
+                            Text("Add Profile")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        .sheet(isPresented: $showNewProfilePage) {
+                            NewProfileView(profiles: $profiles, showNewProfilePage: $showNewProfilePage)
+                        }
+                    } else {
+                        ForEach(profiles.indices, id: \.self) { index in
+                            Button(action: {
+                                selectedProfile = profiles[index]
+                            }) {
+                                Text(profiles[index].name)
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .padding()
+                                    .background(selectedProfile == profiles[index] ? Color.blue : Color.gray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                        }
+                        
+                        Button(action: {
+                            showNewProfilePage = true
+                        }) {
+                            Text("Add Profile")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(Color.green)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        .sheet(isPresented: $showNewProfilePage) {
+                            NewProfileView(profiles: $profiles, showNewProfilePage: $showNewProfilePage)
+                        }
+                        
+                        if let selectedProfile = selectedProfile, let selectedProfileIndex = profiles.firstIndex(of: selectedProfile) {
+                            TextField("Name", text: $profiles[selectedProfileIndex].name)
+                            TextField("Phone Number", text: $profiles[selectedProfileIndex].phoneNumber)
+                            TextField("Address", text: $profiles[selectedProfileIndex].address)
+                        } else {
+                            Text("No profile selected")
+                        }
+
+
+                    }
+                }
+            }
+        }
+        .navigationBarTitle(Text("Settings"))
+    }
 }
 
 struct NewProfileView: View {
- @Binding var profiles: [Profile]
- @State private var newProfileName = ""
- @State private var newProfilePhoneNumber = ""
- @State private var newProfileAddress = ""
- 
- var body: some View {
-     NavigationView {
-         Form {
-             Section(header: Text("New Profile")) {
-                 TextField("Name", text: $newProfileName)
-                 TextField("Phone Number", text: $newProfilePhoneNumber)
-                 TextField("Address", text: $newProfileAddress)
-             }
-             
-             Section {
-                 Button(action: {
-                     let newProfile = Profile(name: newProfileName, phoneNumber: newProfilePhoneNumber, address: newProfileAddress)
-                     profiles.append(newProfile)
-                     newProfileName = ""
-                     newProfilePhoneNumber = ""
-                     newProfileAddress = ""
-                 }) {
-                     Text("Save")
-                         .foregroundColor(.blue)
-                 }
-             }
-         }
-         .navigationBarTitle(Text("New Profile"))
-     }
- }
+    @Binding var profiles: [Profile]
+    @Binding var showNewProfilePage: Bool
+    @State private var newProfileName = ""
+    @State private var newProfilePhoneNumber = ""
+    @State private var newProfileAddress = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("New Profile")) {
+                    TextField("Name", text: $newProfileName)
+                    TextField("Phone Number", text: $newProfilePhoneNumber)
+                    TextField("Address", text: $newProfileAddress)
+                }
+                
+                Section {
+                    Button(action: {
+                        let newProfile = Profile(name: newProfileName, phoneNumber: newProfilePhoneNumber, address: newProfileAddress)
+                        profiles.append(newProfile)
+                        newProfileName = ""
+                        newProfilePhoneNumber = ""
+                        newProfileAddress = ""
+                        showNewProfilePage = false
+                    }) {
+                        Text("Save")
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+            .navigationBarTitle(Text("New Profile"))
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        showNewProfilePage = false
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                            Text("Back")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
-struct SettingsContentView: View {
- var body: some View {
-     SettingsView()
- }
-}
-
-struct SettingsContentView_Previews: PreviewProvider {
- static var previews: some View {
-     ContentView()
- }
-}
-
-
